c3ffd076746b7dab5938d37fd9fa8bd3f0faca6a
benmackenzie <ben.mackenzie@thinkbiganalytics.com>
2018-03-23 13:20:48 -0400

    updated to use latest image

---
+++ README.md
@@ -0,0 +1,3 @@
+how to tell whether to delete a deployment or not?
+how to get the latest version of container?
+how to support rollback.
--- hellonode/server.js
+++ hellonode/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello you Bastard!');
+  response.end('Hello you rotten Bastard!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
