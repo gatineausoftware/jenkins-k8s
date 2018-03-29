f88f3460e7c7371458d3a7faac27cec47e0e0be0
benmackenzie <ben.mackenzie@thinkbiganalytics.com>
2018-03-29 06:45:55 -0400

    change message

--- hellonode/server.js
+++ hellonode/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello you rotten Bastard!');
+  response.end('Hello you rotten Bastardss!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
